1974cefde32a1914962504f0fa89f087b51c1c84
Andy <andymg@gmail.com>
2019-02-21 10:35:39 +0000

    Dynamic Duplicate Input Elements (module 7)

--- Demo/src/app/customers/customer.component.ts
+++ Demo/src/app/customers/customer.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn  } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  ValidatorFn,
+  FormArray
+} from '@angular/forms';
 
 import { Customer } from './customer';
 import { debounceTime } from 'rxjs/operators';
 
-function emailMatcher(c: AbstractControl): { [key: string]: boolean | } | null {
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
   const emailControl = c.get('email');
   const confirmControl = c.get('confirmEmail');
 
@@ -16,13 +23,20 @@ function emailMatcher(c: AbstractControl): { [key: string]: boolean | } | null {
     return null;
   }
 
-  return { 'match': true };
+  return {
+    match: true
+  };
 }
 
-function ratingRange(min: number, max: number) : ValidatorFn {
-  return (control: AbstractControl): { [key: string]: boolean} | null => {
-    if (control.value !== null && (isNaN(control.value) || control.value < min || control.value > max)) {
-      return { 'range': true };
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: boolean } | null => {
+    if (
+      control.value !== null &&
+      (isNaN(control.value) || control.value < min || control.value > max)
+    ) {
+      return {
+        range: true
+      };
     }
 
     return null;
@@ -39,47 +53,68 @@ export class CustomerComponent implements OnInit {
   customer = new Customer();
   emailMessage: string;
 
+  get addresses(): FormArray {
+    return <FormArray>this.customerForm.get('addresses');
+  }
+
   private validationMessages = {
     required: 'Please enter your email address.',
     email: 'Please enter a valid email address.'
   };
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.customerForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
-      emailGroup: this.fb.group({
+      emailGroup: this.fb.group(
+        {
           email: ['', [Validators.required, Validators.email]],
-          confirmEmail: ['', Validators.required],
+          confirmEmail: ['', Validators.required]
         },
-        {validator: emailMatcher}
+        {
+          validator: emailMatcher
+        }
       ),
       phone: '',
       notification: 'email',
       rating: [null, ratingRange(1, 5)],
-      sendCatalog: true
+      sendCatalog: true,
+      addresses: this.fb.array([this.buildAddress()])
     });
 
-    this.customerForm.get('notification').valueChanges.subscribe(
-      value => this.setNotification(value)
-    );
+    this.customerForm
+      .get('notification')
+      .valueChanges.subscribe(value => this.setNotification(value));
 
     const emailControl = this.customerForm.get('emailGroup.email');
-    emailControl.valueChanges.pipe(
-      debounceTime(500)
-    ).subscribe(
-      value => this.setMessage(emailControl)
-    );
+    emailControl.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe(value => this.setMessage(emailControl));
+  }
+
+  addAddress(): void {
+    this.addresses.push(this.buildAddress());
+  }
+
+  buildAddress(): FormGroup {
+    return this.fb.group({
+      addressType: 'home',
+      street1: '',
+      street2: '',
+      city: '',
+      state: '',
+      zip: ''
+    });
   }
 
   populateTestData() {
     // setValue MUST update all properties of the form
     // patchValue can set any or all properties of the form
     this.customerForm.patchValue({
-      firstName: "Jack",
-      lastName: "Harkness",
+      firstName: 'Jack',
+      lastName: 'Harkness',
       sendCatalog: true
     });
   }
@@ -91,16 +126,16 @@ export class CustomerComponent implements OnInit {
 
   setMessage(c: AbstractControl): void {
     this.emailMessage = '';
-    if((c.touched || c.dirty) && c.errors) {
+    if ((c.touched || c.dirty) && c.errors) {
       this.emailMessage = Object.keys(c.errors)
-                                .map(key => this.emailMessage += this.validationMessages[key])
-                                .join(' ');
+        .map(key => (this.emailMessage += this.validationMessages[key]))
+        .join(' ');
     }
   }
 
   setNotification(notifyVia: string): void {
     const phoneControl = this.customerForm.get('phone');
-    if (notifyVia === 'text'){
+    if (notifyVia === 'text') {
       phoneControl.setValidators(Validators.required);
     } else {
       phoneControl.clearValidators();
